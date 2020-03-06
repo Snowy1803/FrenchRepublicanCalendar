4503723561a192ba111dd9bec0f1e2c609e975dc
Snowy_1803 <3mille.prenom.nom@gmail.com>
2020-03-06 22:11:11 +0100

    Initial Commit

---
+++ FrenchRepublicanCalendar WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FrenchRepublicanCalendar WatchKit Extension
+//
+//  Created by Emil on 06/03/2020.
+//  Copyright © 2020 Snowy_1803. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FrenchRepublicanCalendar WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  FrenchRepublicanCalendar WatchKit Extension
+//
+//  Created by Emil on 06/03/2020.
+//  Copyright © 2020 Snowy_1803. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
