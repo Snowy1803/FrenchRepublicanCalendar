5f056240e3bba43112776a2a15fff778f6dc17b9
Snowy <3mille.prenom.nom@gmail.com>
2021-05-29 20:42:26 +0200

    Add a decimal time widget

--- iOS/ContentView.swift
+++ iOS/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
             ScrollView {
                 VStack {
                     TodayWidget()
+                    DecimalTimeWidget()
                     ConverterWidget()
                     if #available(iOS 14, *) {
                         WheelConverterWidget()
---
+++ iOS/DecimalTimeWidget.swift
@@ -0,0 +1,48 @@
+//
+//  DecimalTimeWidget.swift
+//  FrenchRepublicanCalendar
+//
+//  Created by Emil Pedersen on 29/05/2021.
+//  Copyright © 2021 Snowy_1803. All rights reserved.
+//
+
+import SwiftUI
+import FrenchRepublicanCalendarCore
+
+struct DecimalTimeWidget: View {
+    var body: some View {
+        HomeWidget {
+            Image.decorative(systemName: "clock")
+            Text("Heure décimale")
+            Spacer()
+        } content: {
+            HStack {
+                CurrentDecimalTime()
+                Spacer()
+            }
+        }
+    }
+}
+
+struct CurrentDecimalTime: View {
+    let timer = Timer.publish(every: DecimalTime.decimalSecond / 10, on: .main, in: .common).autoconnect()
+    
+    @State private var time = DecimalTime()
+    
+    var body: some View {
+        (
+            Text(time.description)
+                .font(.largeTitle.monospacedDigit())
+            + Text(String(format: "%.1f", time.remainder).dropFirst())
+                .font(.body.monospacedDigit())
+        ).onReceive(timer) { _ in
+            self.time = DecimalTime()
+        }
+    }
+}
+
+struct DecimalTimeWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        DecimalTimeWidget()
+    }
+}
