61cb2f9b7ee8b69c88eccf5b221e39becd04e8ee
Emil Pedersen <emil.pedersen@epitech.eu>
2025-12-04 05:44:05 +0100

    Add Legal section to SettingsView

--- iOS/Screens/SettingsView.swift
+++ iOS/Screens/SettingsView.swift
@@ -15,6 +15,8 @@ import FrenchRepublicanCalendarCore
 import StoreKit
 
 struct SettingsView: View {
+    /// This is separated to avoid GitHub scrapers. If you are looking for email addresses, please disregard this one.
+    static let personalWebsite = "emil.codes"
     @EnvironmentObject var midnight: Midnight
     
     var body: some View {
@@ -50,6 +52,31 @@ struct SettingsView: View {
                 Text("Afficher l'écran de bienvenue")
             }
             #endif
+            
+            Section(header: Text("Légal"), footer: Text("Nous ne traitons pas vos données personelles. Vos données restent en sécurité sur votre appareil.")) {
+                NavigationLink {
+                    Form {
+                        Link("swiftui-introspect", destination: URL(string: "https://github.com/siteline/swiftui-introspect/blob/main/LICENSE")!)
+                    }.navigationTitle(Text("Licenses open source"))
+                } label: {
+                    Text("Licenses open source")
+                }
+                NavigationLink {
+                    Form {
+                        Section(footer: Text("L'application est entièrement open source. Développée en intégralité par Emil Pedersen")) {
+                            Link("Code source de l'application", destination: URL(string: "https://github.com/Snowy1803/FrenchRepublicanCalendar")!)
+                            Link("Code source du convertisseur", destination: URL(string: "https://github.com/Snowy1803/FrenchRepublicanCalendarCore")!)
+                        }
+                        Section(header: Text("Sources astronomiques pour le modèle Delambre"), footer: Text("P. ROCHER, © INSTITUT DE MÉCANIQUE CÉLESTE ET DE CALCUL DES ÉPHÉMÉRIDES – OBSERVATOIRE DE PARIS")) {
+                            Link("Équinoxe d'automne de 1583 à 2999", destination: URL(string: "https://www.imcce.fr/newsletter/docs/Equinoxe_automne_1583_2999.pdf")!)
+                        }
+                    }.navigationTitle(Text("Crédits"))
+                } label: {
+                    Text("Crédits")
+                }
+                Link("Rapporter un problème", destination: URL(string: "https://github.com/Snowy1803/FrenchRepublicanCalendar/issues/new")!)
+                Link("Nous contacter", destination: URL(string: "mailto:frc@\(Self.personalWebsite)?subject=Application Calendrier Républicain")!)
+            }
         }.navigationBarTitle(Text("Réglages"))
         .listNotTooWide()
         .onDisappear {
