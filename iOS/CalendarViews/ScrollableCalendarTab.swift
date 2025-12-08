adf01b429f8df4522b5d6dad5f5c8c40be61ef7b
Emil Pedersen <emil.pedersen@epitech.eu>
2025-12-08 03:55:17 +0100

    Add transitions when changing timescale in calendar tab

--- iOS/CalendarViews/ScrollableCalendarTab.swift
+++ iOS/CalendarViews/ScrollableCalendarTab.swift
@@ -20,36 +20,42 @@ struct ScrollableCalendarTab: View {
     @State private var topItem: FrenchRepublicanDate = FrenchRepublicanDate(date: .now)
 
     var body: some View {
-        switch page {
-        case .year:
-            ScrollableYearView(initialLocation: topItem) { month in
-                topItem = month
-                page = .month
-            }
-        case .month:
-            ScrollableMonthView(topItem: $topItem, selection: Binding {
-                FrenchRepublicanDate(date: .now)
-            } set: {
-                topItem = $0
-                page = .day
-            }) { year in
-                topItem = year
-                page = .year
-            }
-        case .day:
-            DateDetails(date: topItem)
-                .toolbar {
-                    ToolbarItem(placement: .navigation) {
-                        Button {
-                            page = .month
-                        } label: {
-                            HStack {
-                                Image(systemName: "chevron.backward")
-                                Text(topItem, format: .republicanDate.day(.monthOnly))
-                            }
-                        }.labelStyle(.titleAndIcon)
-                    }
+        ZStack {
+            switch page {
+            case .year:
+                ScrollableYearView(initialLocation: topItem) { month in
+                    topItem = month
+                    page = .month
                 }
-        }
+                .transition(.move(edge: .leading))
+            case .month:
+                ScrollableMonthView(topItem: $topItem, selection: Binding {
+                    FrenchRepublicanDate(date: .now)
+                } set: {
+                    topItem = $0
+                    page = .day
+                }) { year in
+                    topItem = year
+                    page = .year
+                }
+                .transition(.opacity)
+            case .day:
+                DateDetails(date: topItem)
+                    .transition(.move(edge: .trailing))
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar {
+                        ToolbarItem(placement: .navigation) {
+                            Button {
+                                page = .month
+                            } label: {
+                                HStack {
+                                    Image(systemName: "chevron.backward")
+                                    Text(topItem, format: .republicanDate.day(.monthOnly))
+                                }
+                            }.labelStyle(.titleAndIcon)
+                        }
+                    }
+            }
+        }.animation(.easeInOut(duration: 0.2), value: page)
     }
 }
