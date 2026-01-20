a2eb7eb8a5f03316ef9b9d83c25c9d827fa7a2a0
Emil Pedersen <emil.pedersen@epitech.eu>
2026-01-20 14:56:08 +0100

    Fix last day in year sometimes being skipped when scrolling through weeks in ScrollableWeekView

--- iOS/CalendarViews/Day/ScrollableWeekView.swift
+++ iOS/CalendarViews/Day/ScrollableWeekView.swift
@@ -16,11 +16,42 @@ struct ScrollableWeekView: View {
     }
     
     var previous: FrenchRepublicanDate {
-        FrenchRepublicanDate(dayInYear: selection.dayInYear - daysPerPage, year: selection.year)
+        let targetDay = selection.dayInYear - daysPerPage
+        
+        if targetDay < 1 {
+            // Would go before start of year
+            let prevYear = selection.year - 1
+            let prevDate = FrenchRepublicanDate(dayInYear: 1, year: prevYear)
+            let lastPage = lastPage(item: prevDate)
+            let targetDay = (lastPage * daysPerPage) + 1 + currentIndex
+            let maxDay = prevDate.dayCountThisYear
+            return FrenchRepublicanDate(dayInYear: min(targetDay, maxDay), year: prevYear)
+        }
+        
+        return FrenchRepublicanDate(dayInYear: targetDay, year: selection.year)
     }
     
     var next: FrenchRepublicanDate {
-        FrenchRepublicanDate(dayInYear: selection.dayInYear + daysPerPage, year: selection.year)
+        let targetDay = selection.dayInYear + daysPerPage
+        let maxDay = selection.dayCountThisYear
+        
+        if targetDay > maxDay {
+            // Would go past end of year
+            let currentPage = page(item: selection)
+            let lastPage = lastPage(item: selection)
+            if currentPage == lastPage {
+                // Already on last page, go to next year
+                let nextYear = selection.year + 1
+                return FrenchRepublicanDate(dayInYear: 1 + currentIndex, year: nextYear)
+            } else {
+                // Not on last page, go to last page of current year
+                let firstDayOfLastPage = (lastPage * daysPerPage) + 1
+                let targetDay = firstDayOfLastPage + currentIndex
+                return FrenchRepublicanDate(dayInYear: min(targetDay, maxDay), year: selection.year)
+            }
+        }
+        
+        return FrenchRepublicanDate(dayInYear: targetDay, year: selection.year)
     }
 
     var body: some View {
@@ -34,5 +65,17 @@ struct ScrollableWeekView: View {
     func row(item: FrenchRepublicanDate) -> Int {
         let number = item.components.day! - 1
         return number / daysPerPage
+    }
+    
+    var currentIndex: Int {
+        (selection.dayInYear - 1) % daysPerPage
+    }
+    
+    func page(item: FrenchRepublicanDate) -> Int {
+        (item.dayInYear - 1) / daysPerPage
+    }
+    
+    func lastPage(item: FrenchRepublicanDate) -> Int {
+        (item.dayCountThisYear - 1) / daysPerPage
     }
 }
